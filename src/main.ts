74dcfc17c324e6ba402c77dd2dafe0b3adfeb73a
403NotFound <1214972923@qq.com>
2020-09-16 22:24:51 +0800

    chore: 新增border.css & reset.css & fastclick

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+// 解决移动端触控 300ms 问题
+import fastClick from 'fastclick';
+import '@/assets/styles/border.css';
+import '@/assets/styles/reset.css';
 
 Vue.config.productionTip = false;
+
+fastClick.attach(document.body);
 
 new Vue({
   router,
