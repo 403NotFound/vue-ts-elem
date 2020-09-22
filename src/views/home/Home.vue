494298223408efc3dde6abfe8d2a93356a5b173b
张磊 <zhang_lei0623@163.com>
2020-09-22 18:27:20 +0800

    feat: 新增百度地图检索周边POI

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,6 +1,16 @@
 <template>
   <div>
-    Home Page
+    <input type="text" v-model="keyword">
+    <baidu-map class="baidu-map">
+      <!-- <bm-view class="map"></bm-view> -->
+      <bm-local-search 
+        class="map-list" 
+        :keyword="keyword" 
+        :auto-viewport="true" 
+        :location="location"
+        @searchcomplete="handleSearchComplete"
+      ></bm-local-search>
+    </baidu-map>
   </div>
 </template>
 
@@ -9,8 +19,25 @@ import HomeHeader from './components/Header.vue'
 
 export default {
   name: 'Home',
+  data() {
+    return {
+      keyword: '杭州',
+      location: '杭州'
+    }
+  },
   components: {
     HomeHeader
+  },
+  methods: {
+    handleSearchComplete(e) {
+      console.log(e);
+    }
   }
 }
 </script>
+
+<style lang="stylus" scoped>
+.map-list
+  width 100%
+  height 2rem
+</style>
